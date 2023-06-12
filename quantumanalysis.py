fc777ba472f730baffac2534ab6971722ba48b29
MGASALUCAS <mgasa.loucat1@gmail.com>
2023-06-12 11:58:51 +0300

    6 commit

--- quantumanalysis.py
+++ quantumanalysis.py
@@ -37,9 +37,9 @@ def find_cat_cont_columns(df): ## Logic to Separate Continuous & Categorical Col
     return cont_columns, cat_columns
 
 ### Web App / Dashboard Code
-st.set_page_config(page_icon=":bar_chart:", page_title="EDA Automated using Python")
+st.set_page_config(page_icon=":bar_chart:", page_title="EDA Automated")
 
-st.title("Harnessing Power of AI, Automate Exploratory Data Analysis Built by QuantumX :bar_chart: :coffee:")
+st.title("Harnessing Power of AI, Automate Exploratory Data Analysis Built by QuantumX :bar_chart:")
 st.caption("Our Goal is to Automate Exploratory data analysis process for data science teams, Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. ", unsafe_allow_html=True)
 st.markdown("Please feel free to connect with us if you want to learn & create better data quality pipelines for better AI systems.", unsafe_allow_html=True)
 upload = st.file_uploader(label="Upload File Here:", type=["csv"])
