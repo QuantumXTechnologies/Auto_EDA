5a55327ab94886f819936e3f62e80506c9d931d4
MGASALUCAS <mgasa.loucat1@gmail.com>
2023-06-12 11:47:35 +0300

    6 commit

--- quantumanalysis.py
+++ quantumanalysis.py
@@ -39,9 +39,9 @@ def find_cat_cont_columns(df): ## Logic to Separate Continuous & Categorical Col
 ### Web App / Dashboard Code
 st.set_page_config(page_icon=":bar_chart:", page_title="EDA Automated using Python")
 
-st.title("Harnessing Power of AI, Automate Exploratory Data Analysis Built Under QuantumX :bar_chart: :coffee:")
-st.caption("Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. ", unsafe_allow_html=True)
-st.markdown("Please feel free to connect with us here if you want to learn how to create data analysis for better data in AI systems.", unsafe_allow_html=True)
+st.title("Harnessing Power of AI, Automate Exploratory Data Analysis Built by QuantumX :bar_chart: :coffee:")
+st.caption("Our Goal is to Automate Exploratory data analysis process for data science teams, Upload CSV file to see various Charts related to EDA. Please upload file that has both continuous columns and categorical columns. Once you upload file, various charts, widgets and basic stats will be displayed. ", unsafe_allow_html=True)
+st.markdown("Please feel free to connect with us if you want to learn how to create data analysis for better data in AI systems.", unsafe_allow_html=True)
 upload = st.file_uploader(label="Upload File Here:", type=["csv"])
 
 if upload: ## File as Bytes
